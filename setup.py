74f93d1af8e963f05423911ed26897149339fc5d
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-02-10 16:58:11 +0100

    Create setup.py
    needs to be  updated with all the libraries

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# version
+here = Path(__file__).absolute().parent
+version_data = {}
+with open(here.joinpath("engine-lib", "__init__.py"), "r") as f:
+    exec(f.read(), version_data)
+version = version_data.get("__version__", "0.0")
+
+install_requires = [
+    "numpy>=1.19",  # 1.19 required by tensorflow 2.6
+    "pandas>1.0.3,<2",
+    "ipykernel>=4.6.1,<7",
+    "jupyter>=1,<2",
+  
+]
+
+setup(
+    name="engine-lib",
+    version=version,
+    install_requires=install_requires,
+    package_dir={"miguellib": "miguellib"},
+    python_requires=">=3.6, <3.10",
+    packages=find_packages(where=".", exclude=["docs", "examples", "tests"]),
